5484d79f5b090943ec923621d0514af6773ac510
Aditya Singh Rathore <aditya18007@iiitd.ac.in>
2020-11-20 16:33:01 +0530

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,87 @@
+#include "utils.h"
+
+int width = 1800, height=1000;
+#define DIVISIONS 100
+#define PI 3.14
+
+int main(int, char**)
+{
+    // Setup window
+    GLFWwindow *window = setupWindow(width, height);
+    ImGuiIO& io = ImGui::GetIO(); // Create IO object
+
+    ImVec4 clearColor = ImVec4(0.0f, 0.0f, 0.0f, 1.00f);
+
+    unsigned int shaderProgram = createProgram("./shaders/vshader.vs", "./shaders/fshader.fs");
+    
+    int vVertex_attrib = glGetAttribLocation(shaderProgram, "vVertex");
+    if(vVertex_attrib == -1) {
+        std::cout << "Could not bind location: vVertex\n" ;
+        exit(0);
+    }else{
+        std::cout << "vVertex found at location " << vVertex_attrib << std::endl;
+    }
+
+    float vertices[] = {
+       1.0f,  1.0f, 0.0f,  // top right
+       1.0f, -1.0f, 0.0f,  // bottom right
+      -1.0f, -1.0f, 0.0f,  // bottom left
+      -1.0f,  1.0f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        glUseProgram(shaderProgram);
+
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Information");                          
+            ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+    cleanup(window);
+    return 0;
+}
